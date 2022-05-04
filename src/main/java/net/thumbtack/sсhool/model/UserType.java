8d03966424e9c54a7e4c163d5c9433d2bb57ed33
Александр Кирносов <serbii2010@gmal.com>
2022-05-04 20:46:40 +0600

    add model from account

---
+++ src/main/java/net/thumbtack/sсhool/model/UserType.java
@@ -0,0 +1,13 @@
+package net.thumbtack.sсhool.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserType {
+    private int id;
+    private String type;
+}
